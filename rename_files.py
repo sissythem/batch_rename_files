e0ad183d6d04b6ebc4af145eeaf4328cdc10aea6
cthem <c.themeli@gmail.com>
2018-08-30 12:14:55 +0300

    add replace a character strategy

--- rename_files.py
+++ rename_files.py
@@ -1,23 +1,7 @@
-
 import os
 import utils
 
-strategies = ["prefix", "suffix", "infix", "replace"]
-
-
-def rename_files():
-    json_properties = utils.read_configurations("properties.json")
-    directory = json_properties["path"]
-    rename_strategy = json_properties["rename_strategy"]["strategy"]
-    name_to_add = json_properties["new_name"]
-    if rename_strategy == strategies[0]:
-        prefix_rename_strategy(directory, name_to_add)
-    elif rename_strategy == strategies[1]:
-        suffix_rename_strategy(directory, name_to_add)
-    elif rename_strategy == strategies[2]:
-        infix_rename_strategy(json_properties, name_to_add)
-    elif rename_strategy == strategies[3]:
-        replace_rename_strategy(directory, name_to_add)
+strategies = ["prefix", "suffix", "infix", "replace_all", "replace_in_position"]
 
 
 def prefix_rename_strategy(directory, name_to_add):
@@ -32,9 +16,10 @@ def suffix_rename_strategy(directory, name_to_add):
         os.rename(os.path.join(directory, f), os.path.join(directory, str(old_name) + name_to_add + str(suffix)))
 
 
-def infix_rename_strategy(json_properties, name_to_add):
+def infix_rename_strategy():
     directory = json_properties["path"]
     position = json_properties["rename_strategy"]["position"]
+    name_to_add = json_properties["new_name"]
     for f in os.listdir(directory):
         suffix, name_parts = utils.split_filename_suffix(f)
         first_part, second_part = utils.split_filename_in_position(position, name_parts)
@@ -42,10 +27,30 @@ def infix_rename_strategy(json_properties, name_to_add):
         os.rename(os.path.join(directory, f), os.path.join(directory, final_name))
 
 
-def replace_rename_strategy(directory, name_to_add):
-    [os.rename(os.path.join(directory, f), os.path.join(directory, name_to_add + str(i) + utils.split_filename_suffix(f)[0]))
+def replace_all_rename_strategy(directory, new_name):
+    [os.rename(os.path.join(directory, f), os.path.join(directory, new_name + str(i) + utils.split_filename_suffix(f)[0]))
      for i, f in enumerate(os.listdir(directory))]
 
 
+def replace_in_position_strategy():
+    directory = json_properties["path"]
+    position = json_properties["rename_strategy"]["position"]
+    name_to_add = json_properties["new_name"]
+    for f in os.listdir(directory):
+        filename = f[:position] + name_to_add + f[position + 1:]
+        os.rename(os.path.join(directory, f), os.path.join(directory, filename))
+
+
 if __name__ == '__main__':
-    rename_files()
+    json_properties = utils.read_configurations("properties.json")
+    rename_strategy = json_properties["rename_strategy"]["strategy"]
+    if rename_strategy == strategies[0]:
+        prefix_rename_strategy(json_properties["path"], json_properties["new_name"])
+    elif rename_strategy == strategies[1]:
+        suffix_rename_strategy(json_properties["path"], json_properties["new_name"])
+    elif rename_strategy == strategies[2]:
+        infix_rename_strategy()
+    elif rename_strategy == strategies[3]:
+        replace_all_rename_strategy(json_properties["path"], json_properties["new_name"])
+    elif rename_strategy == strategies[4]:
+        replace_in_position_strategy()
