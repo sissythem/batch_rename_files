43ee8c549c37440e6a6099620089e02b2064a405
cthem <c.themeli@gmail.com>
2018-08-30 13:10:44 +0300

    add separate functionality for directories and files

--- rename_directories.py
+++ rename_directories.py
@@ -6,15 +6,12 @@ strategies = ["prefix", "suffix", "infix", "replace"]
 
 def prefix_rename_strategy(directory, name_to_add):
     [os.rename(os.path.join(directory, f), os.path.join(directory, name_to_add) + str(f)) for f in
-     os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+     os.listdir(directory) if os.path.isdir(os.path.join(directory, f))]
 
 
 def suffix_rename_strategy(directory, name_to_add):
-    for f in os.listdir(directory):
-        if os.path.isfile(os.path.join(directory, f)):
-            suffix, name_parts = utils.split_filename_suffix(f)
-            old_name = "".join(name_parts)
-            os.rename(os.path.join(directory, f), os.path.join(directory, str(old_name) + name_to_add + str(suffix)))
+    [os.rename(os.path.join(directory, f), os.path.join(directory, str(f) + name_to_add)) for f in
+     os.listdir(directory) if os.path.isdir(os.path.join(directory, f))]
 
 
 def infix_rename_strategy():
@@ -22,20 +19,18 @@ def infix_rename_strategy():
     position = json_properties["rename_strategy"]["position"]
     name_to_add = json_properties["new_name"]
     for f in os.listdir(directory):
-        if os.path.isfile(os.path.join(directory, f)):
-            suffix, name_parts = utils.split_filename_suffix(f)
-            first_part, second_part = utils.split_filename_in_position(position, name_parts)
-            final_name = "".join(first_part + list(name_to_add) + second_part) + suffix
+        if os.path.isdir(os.path.join(directory, f)):
+            first_part, second_part = utils.split_filename_in_position(position, list(f))
+            final_name = "".join(first_part + list(name_to_add) + second_part)
             os.rename(os.path.join(directory, f), os.path.join(directory, final_name))
 
 
 def replace_all_rename_strategy(directory, new_name):
     count = 0
     for f in os.listdir(directory):
-        if os.path.isfile(os.path.join(directory, f)):
-            count = count + 1
-            os.rename(os.path.join(directory, f),
-                      os.path.join(directory, new_name + str(count) + utils.split_filename_suffix(f)[0]))
+        if os.path.isdir(os.path.join(directory, f)):
+            count = count+1
+            os.rename(os.path.join(directory, f), os.path.join(directory, new_name + str(count)))
 
 
 def replace_rename_strategy():
@@ -47,10 +42,8 @@ def replace_rename_strategy():
         replace_all_rename_strategy(directory, name_to_add)
         return
     for f in os.listdir(directory):
-        if os.path.isfile(os.path.join(directory, f)):
-            suffix, name_parts = utils.split_filename_suffix(f)
-            old_name = "".join(name_parts)
-            filename = old_name[:position] + name_to_add + old_name[position + characters_to_replace:] + suffix
+        if os.path.isdir(os.path.join(directory, f)):
+            filename = f[:position] + name_to_add + f[position + characters_to_replace:]
             os.rename(os.path.join(directory, f), os.path.join(directory, filename))
 
 
