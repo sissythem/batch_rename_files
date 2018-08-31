c926789dbf185d154547b5de42a4d5df312623c0
cthem <c.themeli@gmail.com>
2018-08-31 11:03:29 +0300

    Update rename_directories.py

--- rename_directories.py
+++ rename_directories.py
@@ -5,16 +5,30 @@ strategies = ["prefix", "suffix", "infix", "replace"]
 
 
 def prefix_rename_strategy(directory, name_to_add):
+    """
+    Add a prefix to the directory name
+    :param directory: the path to the root directory
+    :param name_to_add: the prefix to be added in the directory name
+    """
     [os.rename(os.path.join(directory, f), os.path.join(directory, name_to_add) + str(f)) for f in
      os.listdir(directory) if os.path.isdir(os.path.join(directory, f))]
 
 
 def suffix_rename_strategy(directory, name_to_add):
+    """
+    Add a suffix to the directory name
+    :param directory: the path to the root directory
+    :param name_to_add: the prefix to be added in the directory name
+    """
     [os.rename(os.path.join(directory, f), os.path.join(directory, str(f) + name_to_add)) for f in
      os.listdir(directory) if os.path.isdir(os.path.join(directory, f))]
 
 
 def infix_rename_strategy():
+    """
+    Add some characters inside the directory name
+    :return:
+    """
     directory = json_properties["path"]
     position = json_properties["rename_strategy"]["position"]
     name_to_add = json_properties["new_name"]
@@ -26,6 +40,12 @@ def infix_rename_strategy():
 
 
 def replace_all_rename_strategy(directory, new_name):
+    """
+    Replace the directory name with a new name (in case of multiple directories an incremented number will be
+    added at the end of the name)
+    :param directory: the path to the root directory
+    :param new_name: the prefix to be added in the directory name
+    """
     count = 0
     for f in os.listdir(directory):
         if os.path.isdir(os.path.join(directory, f)):
@@ -34,6 +54,13 @@ def replace_all_rename_strategy(directory, new_name):
 
 
 def replace_rename_strategy():
+    """
+    Replace some (or all the) characters of a directory name with the specified ones.
+    Check if the whole name should be replaced - in this case the replace_all_rename_strategy will be used -
+    otherwise the directory name will be split in the specified position and the characters will be replaced
+    with the new name
+    :return:
+    """
     directory = json_properties["path"]
     position = json_properties["rename_strategy"]["position"]
     name_to_add = json_properties["new_name"]
