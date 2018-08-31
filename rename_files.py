fe054651416ebef98ce9d0c15fefb64dadc8ba2f
cthem <c.themeli@gmail.com>
2018-08-31 11:04:03 +0300

    Add documentation to the rename_files.py

--- rename_files.py
+++ rename_files.py
@@ -5,11 +5,22 @@ strategies = ["prefix", "suffix", "infix", "replace"]
 
 
 def prefix_rename_strategy(directory, name_to_add):
+    """
+    Add a prefix to the filename
+    :param directory: the path to the root directory
+    :param name_to_add: the prefix to be added in the filename
+    """
     [os.rename(os.path.join(directory, f), os.path.join(directory, name_to_add) + str(f)) for f in
      os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
 
 
 def suffix_rename_strategy(directory, name_to_add):
+    """
+    Add a suffix to the filename. This will be added before the suffix with the
+    file type (e.g. test.txt is the initial filename and we want to add _1 => test_1.txt)
+    :param directory: the path to the root directory
+    :param name_to_add: the prefix to be added in the filename
+    """
     for f in os.listdir(directory):
         if os.path.isfile(os.path.join(directory, f)):
             suffix, name_parts = utils.split_filename_suffix(f)
@@ -18,6 +29,10 @@ def suffix_rename_strategy(directory, name_to_add):
 
 
 def infix_rename_strategy():
+    """
+    Add some characters inside the filename
+    :return:
+    """
     directory = json_properties["path"]
     position = json_properties["rename_strategy"]["position"]
     name_to_add = json_properties["new_name"]
@@ -30,6 +45,12 @@ def infix_rename_strategy():
 
 
 def replace_all_rename_strategy(directory, new_name):
+    """
+    Replace the filename with a new name (in case of multiple files an incremented number will be
+    added at the end of the name). The type of the file will remain as is.
+    :param directory: the path to the root directory
+    :param new_name: the prefix to be added in the filename
+    """
     count = 0
     for f in os.listdir(directory):
         if os.path.isfile(os.path.join(directory, f)):
@@ -39,6 +60,13 @@ def replace_all_rename_strategy(directory, new_name):
 
 
 def replace_rename_strategy():
+    """
+    Replace some (or all the) characters of a filename with the specified ones.
+    Check if the whole name should be replaces - in this case the replace_all_rename_strategy will be used -
+    otherwise the filename is split in the position where the characters will be replaced, the new characters
+    are added and at the end of the name the file type is also added
+    :return:
+    """
     directory = json_properties["path"]
     position = json_properties["rename_strategy"]["position"]
     name_to_add = json_properties["new_name"]
